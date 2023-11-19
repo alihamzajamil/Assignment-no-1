e4b9bbbb05fd3d79928972ef2f162aa8626078be
ali hamza <alihamza92123@gmail.com>
2023-11-19 08:08:01 -0800

    add routes

--- travel-website/src/App.js
+++ travel-website/src/App.js
@@ -1,15 +1,51 @@
-import React from "react";
-import Home from "./Components/Home/home";
-import About from "./Components/About/About";
-import Contact from "./Components/Contact/Contact";
-function App() {
-  return (
-    <div className="App">
-      <Home/>
-      <About/>
-      <Contact/>
-    </div>
-  );
-}
+// import React from "react";
+// import Home from "./Components/Home/home";
+// import About from "./Components/About/About";
+// import Contact from "./Components/Contact/Contact";
+// function App() {
+//   return (
+//     <div className="App">
+//       <Home/>
+//       <About/>
+//       <Contact/>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
+
+import React, { Component } from 'react'; 
+import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'; 
+ import Home from "./Components/Home/home";
+ import About from "./Components/About/About";
+ import Contact from "./Components/Contact/Contact";
+
+  
+class App extends Component { 
+  render() { 
+      return ( 
+      <Router> 
+          <div className="App"> 
+              <ul className="App-header"> 
+              <li> 
+                  <Link to="/">Home</Link> 
+              </li> 
+              <li> 
+                  <Link to="/about">About Us</Link> 
+              </li> 
+              <li> 
+                  <Link to="/contact">Contact Us</Link> 
+              </li> 
+              </ul> 
+          <Routes> 
+                  <Route exact path='/' element={< Home />}></Route> 
+                  <Route exact path='/about' element={< About />}></Route> 
+                  <Route exact path='/contact' element={< Contact />}></Route> 
+          </Routes> 
+          </div> 
+      </Router> 
+  ); 
+  } 
+  } 
+    
+  export default App;
